62addf660ed6b9bcd63bae408fca225fce03cb6c
ryanhaus <50347890+ryanhaus@users.noreply.github.com>
2022-09-16 16:19:23 -0400

    use handlebars instead of html for home page

--- src/pages/index_page.rs
+++ src/pages/index_page.rs
@@ -1,8 +1,12 @@
+use handlebars::Handlebars;
 use rocket::{ get, http::ContentType };
 
-const INDEX_FILE_SRC: &'static str = include_str!("templates/img_set.html");
+const INDEX_FILE_SRC: &'static str = include_str!("templates/img_set.html.hbs");
 
 #[get("/")]
-pub fn index_page() -> (ContentType, &'static str) {
-    (ContentType::HTML, INDEX_FILE_SRC)
+pub fn index_page() -> (ContentType, String) {
+    let handlebars = Handlebars::new();
+    let data: Option<i32> = None;
+
+    (ContentType::HTML, handlebars.render_template(INDEX_FILE_SRC, &data).unwrap())
 }
