2d5eff82edf29d5be121d7c29a59ad1191442f11
ryanhaus <50347890+ryanhaus@users.noreply.github.com>
2022-09-19 19:33:12 -0400

    image editing in svg

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -1,21 +1,36 @@
 const image_url = `/file/${config.PAGE_ID}`;
 
+var svg;
+let texts = [], images = [];
+
 window.addEventListener("load", () => {
     let request = new XMLHttpRequest();
     request.onload = function() {
         const content_type = this.getResponseHeader("content-type");
-
         if (content_type != "image/svg" && content_type != "image/svg+xml") is_not_svg();
+        
+        svg = this.responseXML;
 
-        let response = this.responseXML,
-            children = response.children[0].children;
+        // parse texts
+        Array.from(svg.getElementsByTagName("text")).forEach((element, i) => {
+            if (element.children.length != 0)
+                element = element.children[0];
 
-        for (let i = 0; i < children.length; i++)
-            if (children[i].tagName == "text")
-                document.querySelector("span#text_editors").innerHTML +=
-                `<input type="text" value="${children[i].innerHTML}" oninput="update_text(${i}, this.value)" /><br/>`;
+            texts.push(element);
+            
+            document.querySelector("span#text_editors").innerHTML +=
+                `<input type="text" value="${element.innerHTML}" oninput="update_text(${i}, this.value)" /><br/>`;
+        });
 
-        set_image(response);
+        // parse images
+        Array.from(svg.getElementsByTagName("image")).forEach((element, i) => {
+            images.push(element);
+
+            document.querySelector("span#image_editors").innerHTML +=
+                `<img style="max-width: 512px;" src="${element.attributes["xlink:href"].value}" id="img_${i}" /><br/>Replace: <input type="file" accept="image/*" oninput="update_image(${i}, this.files[0])" /><hr />`;
+        });
+                
+        set_image();
     }
 
     request.onerror = is_not_svg;
@@ -24,18 +39,26 @@ window.addEventListener("load", () => {
     request.send();
 });
 
-function update_text(child, value) {
-    let edit_img = document.querySelector("img#editing_image");
-    let decoded = decodeURIComponent(edit_img.getAttribute("src").substring(24));
-    let xml_src = new DOMParser().parseFromString(decoded, "text/xml");
-
-    xml_src.children[0].children[child].innerHTML = value;
-
-    set_image(xml_src);
+function update_text(index, value) {
+    texts[index].innerHTML = value;
+    set_image();
 }
 
-function set_image(xml) {
-    let string_src = new XMLSerializer().serializeToString(xml);
+function update_image(index, value) {
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        images[index].setAttribute("xlink:href", e.target.result);
+        document.querySelector(`img#img_${index}`).setAttribute("src", e.target.result);
+
+        set_image();
+    }
+
+    reader.readAsDataURL(value);
+}
+
+function set_image() {
+    let string_src = new XMLSerializer().serializeToString(svg);
     document.querySelector("img#editing_image").setAttribute("src", `data:image/svg+xml;utf8,${encodeURIComponent(string_src)}`);
 }
 
